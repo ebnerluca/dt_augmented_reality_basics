ca5cf5ee302680c670fc1691ad039f623f329942
Luca Ebner <ebnerl@student.ethz.ch>
2020-11-10 15:58:11 +0100

    Augmenter class, restructuring.

--- packages/augmented_reality_basics/src/augmented_reality_basics_node.py
+++ packages/augmented_reality_basics/src/augmented_reality_basics_node.py
@@ -7,10 +7,11 @@ import yaml
 from duckietown.dtros import DTROS, NodeType, TopicType, DTParam, ParamType
 #from duckietown_msgs.msg import Twist2DStamped, WheelEncoderStamped, WheelsCmdStamped
 from std_msgs.msg import Header, Float32
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import CompressedImage, CameraInfo
 #from std_srvs.srv import Trigger, TriggerResponse
 import cv2
 from cv_bridge import CvBridge
+from augmenter import Augmenter
 
 class AugmentedRealityBasics(DTROS):
 
@@ -29,14 +30,14 @@ class AugmentedRealityBasics(DTROS):
         # Intrinsics
         rospy.loginfo("[AugmentedRealityBasics]: Loading Camera Calibration Intrinsics ...")
 
-        if(not os.path.isfile(f'/data/config/calibrations/camera_intrinsic/{self.veh_name}.yaml')):
-            rospy.logwarn(f'[AugmentedRealityBasics]: Could not find {self.veh_name}.yaml. Loading default.yaml')
-            self.camera_calibration_intrinsics = self.read_yaml_file(f'/data/config/calibrations/camera_intrinsic/default.yaml')
-        else:
-            self.camera_calibration_intrinsics = self.read_yaml_file(f'/data/config/calibrations/camera_intrinsic/{self.veh_name}.yaml')
+        #if(not os.path.isfile(f'/data/config/calibrations/camera_intrinsic/{self.veh_name}.yaml')):
+        #    rospy.logwarn(f'[AugmentedRealityBasics]: Could not find {self.veh_name}.yaml. Loading default.yaml')
+        #    self.camera_calibration_intrinsics = self.read_yaml_file(f'/data/config/calibrations/camera_intrinsic/default.yaml')
+        #else:
+        #    self.camera_calibration_intrinsics = self.read_yaml_file(f'/data/config/calibrations/camera_intrinsic/{self.veh_name}.yaml')
         
-        self._K, self._D, self._R, self._P, self.cam_width, self.cam_height, self._distortion_model = self.extract_camera_data(self.camera_calibration_intrinsics)
-        self._K_rect, self._roi = cv2.getOptimalNewCameraMatrix(self._K, self._D, (self.cam_width, self.cam_height), 1)
+        # self._K, self._D, self._R, self._P, self.cam_width, self.cam_height, self._distortion_model = self.extract_camera_data(self.camera_calibration_intrinsics)
+        # self._K_rect, self._roi = cv2.getOptimalNewCameraMatrix(self._K, self._D, (self.cam_width, self.cam_height), 1)
         #rospy.loginfo(f"[AugmentedRealityBasics]: roi: {self._roi}")
         #rospy.loginfo(f"[AugmentedRealityBasics]: K_rect: \n{self._K_rect}")
         #rospy.loginfo(f"[AugmentedRealityBasics]: K: \n{self._K}")
@@ -45,6 +46,14 @@ class AugmentedRealityBasics(DTROS):
         #rospy.loginfo(f"[AugmentedRealityBasics]: P: \n{self._P}")
         #rospy.loginfo(f"[AugmentedRealityBasics]: cam_width: {self.cam_width}")
         #rospy.loginfo(f"[AugmentedRealityBasics]: cam_height: {self.cam_height}")
+        if(not os.path.isfile(f'/data/config/calibrations/camera_intrinsic/{self.veh_name}.yaml')):
+            rospy.logwarn(f'[AugmentedRealityBasics]: Could not find {self.veh_name}.yaml. Loading default.yaml')
+            camera_intrinsic = self.read_yaml_file(f'/data/config/calibrations/camera_intrinsic/default.yaml')
+        else:
+            camera_intrinsic = self.read_yaml_file(f'/data/config/calibrations/camera_intrinsic/{self.veh_name}.yaml')
+        camera_info = self.camera_info_from_yaml(camera_intrinsic)
+        rospy.loginfo(f"[AugmentedRealityBasics]: camera_info = {camera_info}")
+
 
         # Extrinsics
         rospy.loginfo("[AugmentedRealityBasics]: Loading Camera Calibration Extrinsics ...")
@@ -55,18 +64,33 @@ class AugmentedRealityBasics(DTROS):
         else:
             extrinsics = self.read_yaml_file(f'/data/config/calibrations/camera_extrinsic/{self.veh_name}.yaml')
         
-        self.camera_calibration_extrinsics = np.array(extrinsics["homography"]).reshape(3,3)
-        self.camera_calibration_extrinsics_inv = np.linalg.inv(self.camera_calibration_extrinsics)
-        #rospy.loginfo(f"[AugmentedRealityBasics]: Extrinsics: {self.camera_calibration_extrinsics}")
+        #self.camera_calibration_extrinsics = np.array(extrinsics["homography"]).reshape(3,3)
+        #self.camera_calibration_extrinsics_inv = np.linalg.inv(self.camera_calibration_extrinsics)
+        homography = np.array(extrinsics["homography"]).reshape(3,3) #homography that maps axle coordinates to image frame coordinates
+        homography = np.linalg.inv(homography)
+        rospy.loginfo(f"[AugmentedRealityBasics]: homography: {homography}")
         
         
-        #Load Map
+        # Augmenter class
+        rospy.loginfo("[AugmentedRealityBasics]: Initializing Subscribers ...")
+        self.augmenter = Augmenter(camera_info, homography, debug=False)
+
+        
+        # Load Map
         rospy.loginfo("[AugmentedRealityBasics]: Loading Map ...")
         self.map_name = os.environ.get('MAP_FILE', 'hud.yaml').strip(".yaml")
         rospy.loginfo("[AugmentedRealityBasics]: Map Name: %s" %self.map_name)
         self.map_dict = self.read_yaml_file(os.environ.get('DT_REPO_PATH', '/') + '/packages/augmented_reality_basics/maps/' + self.map_name + '.yaml')
 
-        
+
+        # Remap points in map_dict from their reference frame to image frame
+        self.remap_points(self.map_dict["points"])
+
+
+        # CV bridge
+        self.cv_bridge = CvBridge()
+
+
         # Subscribers
         rospy.loginfo("[AugmentedRealityBasics]: Initializing Subscribers ...")
         self.image_subscriber = rospy.Subscriber('camera_node/image/compressed', CompressedImage, self.callback)
@@ -77,46 +101,65 @@ class AugmentedRealityBasics(DTROS):
         self.mod_img_pub = rospy.Publisher(f'augmented_reality_basics_node/{self.map_name}/image/compressed', CompressedImage, queue_size=1)
 
         
-        self.cv_bridge = CvBridge()
-        
-
-        self.ground2pixel( self.map_dict['points'] )
-
-        
         rospy.loginfo("[AugmentedRealityBasics]: Initialized.")
 
+    
     def callback(self, imgmsg):
 
-        #convert msg to cv2
+        # Convert msg to cv2
         img = self.cv_bridge.compressed_imgmsg_to_cv2(imgmsg)
 
-        #process image
-        undistorted_image = self.process_image(img)
+        # Process image
+        #undistorted_image = self.process_image(img)
+        undistorted_image = self.augmenter.process_image(img)
         
-        #project points to img pixels
-        #done only once        
+        # Project points to img pixels
+        # (already done during node init, doesn't need to be redone)
 
-        #render modified image with segments
-        modified_image = self.render_segments(undistorted_image, self.map_dict['segments'])
+        # Render modified image with segments
+        modified_image = self.augmenter.render_segments(undistorted_image, self.map_dict)
         
+        # Create ROS msg
         modified_image_msg = self.cv_bridge.cv2_to_compressed_imgmsg(modified_image)
         modified_image_msg.header.stamp = rospy.Time.now()
 
         self.mod_img_pub.publish(modified_image_msg)
+        
         return
 
-    def render_segments(self, img, segments):
 
-        for seg in segments:
+    def remap_points(self, points_dict):
 
-            pt_1_string = seg['points'][0]
-            pt_1 = self.map_dict['points'][pt_1_string][1]
-            pt_2_string = seg['points'][1]
-            pt_2 = self.map_dict['points'][pt_2_string][1]
+        for item in points_dict.values():
+            frame = item[0]
+            point = item[1]
 
-            self.draw_segment(img, pt_1, pt_2, seg['color'])
+            if (frame=="axle"):
+                item[0] = "image"
+                item[1] = self.augmenter.ground2pixel(point)
+            elif (frame == "image01"):
+                item[0] = "image"
+                item[1] = self.augmenter.image01_to_pixel(point)
+            elif (frame == "image"):
+                pass
+            else:
+                raise Exception(f"[AugmentedRealityBasics.remap_points]: Invalid frame: {frame}")
+            
+        #rospy.loginfo(f"[AugmentedRealityBasics]: Remapped points: {points_dict}")
+    
 
-        return img
+    #def render_segments(self, img, segments):
+
+    #    for seg in segments:
+
+    #        pt_1_string = seg['points'][0]
+    #        pt_1 = self.map_dict['points'][pt_1_string][1]
+    #        pt_2_string = seg['points'][1]
+    #        pt_2 = self.map_dict['points'][pt_2_string][1]
+
+    #        self.draw_segment(img, pt_1, pt_2, seg['color'])
+
+    #    return img
 
     def read_yaml_file(self,fname):
         """
@@ -138,6 +181,23 @@ class AugmentedRealityBasics(DTROS):
                 rospy.signal_shutdown()
                 return
 
+    @staticmethod
+    def camera_info_from_yaml(calib_data):
+        """
+        Express calibration data (intrinsics) as a CameraInfo instance.
+        :param calib_data: dict, loaded from yaml file
+        :return: intrinsics as CameraInfo instance
+        """
+        cam_info = CameraInfo()
+        cam_info.width = calib_data['image_width']
+        cam_info.height = calib_data['image_height']
+        cam_info.K = calib_data['camera_matrix']['data']
+        cam_info.D = calib_data['distortion_coefficients']['data']
+        cam_info.R = calib_data['rectification_matrix']['data']
+        cam_info.P = calib_data['projection_matrix']['data']
+        cam_info.distortion_model = calib_data['distortion_model']
+        return cam_info
+
     
     def run(self):
         rate = rospy.Rate(2.0)
@@ -147,57 +207,57 @@ class AugmentedRealityBasics(DTROS):
             rate.sleep()
 
 
-    def process_image(self, img):
-        """
-        Rectify image
-        """
-        undistorted_img = cv2.undistort(img, self._K, self._D, None, self._K_rect)
-        # Optionally crop image to ROI
-        #x, y, w, h = self._roi
-        #undistorted_img = undistorted_img[y:y + h, x:x + w]
-        return undistorted_img
+    #def process_image(self, img):
+    #    """
+    #    Rectify image
+    #    """
+    #    undistorted_img = cv2.undistort(img, self._K, self._D, None, self._K_rect)
+    #    # Optionally crop image to ROI
+    #    #x, y, w, h = self._roi
+    #    #undistorted_img = undistorted_img[y:y + h, x:x + w]
+    #    return undistorted_img
 
-    def ground2pixel(self, ground_points_dict):
-        """
-        Transforms point list from their reference frame to the image pixels frame.
-        """
-        for point in ground_points_dict.values(): #reference frame = image01
-                
-                frame = point[0]
+    #def ground2pixel(self, ground_points_dict):
+    #    """
+    #    Transforms point list from their reference frame to the image pixels frame.
+    #    """
+    #    for point in ground_points_dict.values(): #reference frame = image01
+    #            
+    #            frame = point[0]
 
-                if(frame == "image01"):
-                    point[0] = "image"
-                    point[1][0] *= self.cam_height
-                    point[1][1] *= self.cam_width
+    #            if(frame == "image01"):
+    #                point[0] = "image"
+    #                point[1][0] *= self.cam_height
+    #                point[1][1] *= self.cam_width
 
-                elif(frame == "axle"):
-                    point[0] = "image"
-                    point_h = np.array([point[1][0], point[1][1], 1.0])
-                    pixel_h = np.dot(self.camera_calibration_extrinsics_inv, point_h) #point_h must be [x,y,1]
-                    pixel_h = pixel_h / pixel_h[2]
-                    pixel = pixel_h[0:2] 
-                    pixel = [int(i) for i in pixel] #pixel index must be integers
-                    point[1][0] = pixel[1]
-                    point[1][1] = pixel[0] #image frame points are [row, collumn]
+    #            elif(frame == "axle"):
+    #                point[0] = "image"
+    #                point_h = np.array([point[1][0], point[1][1], 1.0])
+    #                pixel_h = np.dot(self.camera_calibration_extrinsics_inv, point_h) #point_h must be [x,y,1]
+    #                pixel_h = pixel_h / pixel_h[2]
+    #                pixel = pixel_h[0:2] 
+    #                pixel = [int(i) for i in pixel] #pixel index must be integers
+    #                point[1][0] = pixel[1]
+    #                point[1][1] = pixel[0] #image frame points are [row, collumn]
 
-    @staticmethod
-    def draw_segment(image, start_point, end_point, color):
+    #@staticmethod
+    #def draw_segment(image, start_point, end_point, color):
         
-        defined_colors = {
-            'red': ['rgb', [1, 0, 0]],
-            'green': ['rgb', [0, 1, 0]],
-            'blue': ['rgb', [0, 0, 1]],
-            'yellow': ['rgb', [1, 1, 0]],
-            'magenta': ['rgb', [1, 0 , 1]],
-            'cyan': ['rgb', [0, 1, 1]],
-            'white': ['rgb', [1, 1, 1]],
-            'black': ['rgb', [0, 0, 0]]}
+    #    defined_colors = {
+    #        'red': ['rgb', [1, 0, 0]],
+    #        'green': ['rgb', [0, 1, 0]],
+    #        'blue': ['rgb', [0, 0, 1]],
+    #        'yellow': ['rgb', [1, 1, 0]],
+    #        'magenta': ['rgb', [1, 0 , 1]],
+    #        'cyan': ['rgb', [0, 1, 1]],
+    #        'white': ['rgb', [1, 1, 1]],
+    #        'black': ['rgb', [0, 0, 0]]}
         
-        _color_type, [r, g, b] = defined_colors[color]
+    #    _color_type, [r, g, b] = defined_colors[color]
         
-        cv2.line(image, (start_point[1], start_point[0]), (end_point[1], end_point[0]), (b * 255, g * 255, r * 255), 5)
+    #    cv2.line(image, (start_point[1], start_point[0]), (end_point[1], end_point[0]), (b * 255, g * 255, r * 255), 5)
         
-        return image
+    #    return image
 
     @staticmethod
     def extract_camera_data(data):
---
+++ packages/augmented_reality_basics/src/augmenter.py
@@ -0,0 +1,101 @@
+import numpy as np
+import rospy
+import cv2
+import time
+
+from image_geometry import PinholeCameraModel
+
+
+class Augmenter:
+    """
+    AR utility class. Can be used to rectify camera images and project line segments on it.
+    Heavily inspired by: https://github.com/duckietown/dt-core/blob/952ebf205623a2a8317fcb9b922717bd4ea43c98/packages/image_processing/include/image_processing/rectification.py
+    """
+    def __init__(self, camera_info, homography, debug=False):
+        
+        self.pcm = PinholeCameraModel()
+        self.pcm.fromCameraInfo(camera_info)
+        self.H = homography  # maps points on ground plane to image plane
+        self.debug = debug
+
+        self.mapx, self.mapy = self._init_rectification()
+
+    def _init_rectification(self):
+        """
+        Establish rectification mapping.
+        """
+        w = self.pcm.width
+        h = self.pcm.height
+        mapx = np.ndarray(shape=(h, w, 1), dtype='float32')
+        mapy = np.ndarray(shape=(h, w, 1), dtype='float32')
+        mapx, mapy = cv2.initUndistortRectifyMap(self.pcm.K, self.pcm.D, self.pcm.R,
+                                                 self.pcm.P, (w, h),
+                                                 cv2.CV_32FC1, mapx, mapy)
+        return mapx, mapy
+
+    def process_image(self, img_raw, interpolation=cv2.INTER_NEAREST):
+        """
+        Rectify image.
+        """
+        return cv2.remap(img_raw, self.mapx, self.mapy, interpolation)
+
+
+    def ground2pixel(self, ground_point):
+        """
+        Transforms point list from the axle frame to the image pixel frame.
+        Input: ground_point ([x,y])
+        """
+        point_h = np.array([ground_point[0], ground_point[1], 1.0]) # Homogeneous coordinates
+        self.log(f"[Augmenter]: point_h = {point_h}")
+
+        pixel_h = np.dot(self.H, point_h) # Homography transform
+
+        pixel_h = pixel_h / pixel_h[2]
+        pixel = [int(pixel_h[1]), int(pixel_h[0])] # Pixel must be in [row, column]
+        self.log(f"[Augmenter]: pixel = {pixel}")
+
+        return pixel
+
+
+    def image01_to_pixel(self, point):
+        """
+        Scale point coordinates to match image frame dimensions.
+        """
+        point[0] *= self.pcm.height
+        point[1] *= self.pcm.width
+        return point
+
+    def render_segments(self, img, map_dict):
+        """
+        Render all the segments defined in 'map_dict' onto 'img'.
+        """
+        for seg in map_dict['segments']:
+            pt_1_string = seg['points'][0]
+            pt_1 = map_dict['points'][pt_1_string][1]
+            pt_2_string = seg['points'][1]
+            pt_2 = map_dict['points'][pt_2_string][1]
+            self.draw_segment(img, pt_1, pt_2, seg['color'])
+        return img
+
+    @staticmethod
+    def draw_segment(image, pt_1, pt_2, color):
+        """
+        Draw a line defined by two points into image. The points are assumed to contain [y, x] which
+        corresponds to [row, column] of the image.
+        """
+        defined_colors = {
+            'red': ['rgb', [1, 0, 0]],
+            'green': ['rgb', [0, 1, 0]],
+            'blue': ['rgb', [0, 0, 1]],
+            'yellow': ['rgb', [1, 1, 0]],
+            'magenta': ['rgb', [1, 0, 1]],
+            'cyan': ['rgb', [0, 1, 1]],
+            'white': ['rgb', [1, 1, 1]],
+            'black': ['rgb', [0, 0, 0]]}
+        _color_type, [r, g, b] = defined_colors[color]
+        cv2.line(image, (pt_1[1], pt_1[0]), (pt_2[1], pt_2[0]), (b * 255, g * 255, r * 255), 5)
+        return image
+
+    def log(self, msg):
+        if self.debug:
+            rospy.loginfo(msg)
